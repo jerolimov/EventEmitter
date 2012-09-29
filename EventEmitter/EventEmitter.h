f489781095e4a4167227f811fb12cf57066ac2a6
Christoph Jerolimov <jerolimov+git+2012@gmail.com>
2012-09-29 11:33:18 +0200

    Add License information (Apache License 2.0)

--- EventEmitter/EventEmitter.h
+++ EventEmitter/EventEmitter.h
@@ -2,8 +2,19 @@
 //  EventEmitter.h
 //  EventEmitter
 //
-//  Created by Christoph Jerolimov on 07.09.2012.
-//  Copyright (c) 2012. All rights reserved.
+//  Copyright 2012 Christoph Jerolimov
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License
 //
 
 #import <Foundation/Foundation.h>
@@ -11,7 +22,7 @@
 #import "EventEmitterListener.h"
 
 /*
-Node.js EventEmitter inspired, c-block-listener library.
+Node.js inspired EventEmitter for objective c.
 
 http://nodejs.org/docs/latest/api/events.html
  */
